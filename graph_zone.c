52b11a2ebc24206b785e4af7cd69f60725d0ec36
darshan547 <60703820+darshan547@users.noreply.github.com>
2020-05-18 13:12:50 +0200

    Update graph_zone.c

--- graph_zone.c
+++ graph_zone.c
@@ -1,41 +1,68 @@
-#include "graph_zone.h"
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "graphe_zones.h"
 
 
 
-Cellule_som* ajoute_liste_sommet(Sommet* s , Cellule_som* cs){
-	Cellule_som* ncs = malloc(sizeof(*ncs));
-	ncs->sommet = s;
-	ncs->suiv = cs;
-	return ncs; 
+Cellule_som * ajoute_liste_sommet(Sommet *s, Cellule_som *som){
+    Cellule_som * tmp = (Cellule_som *) malloc(sizeof(Cellule_som));
+    
+    tmp->sommet = s;
+    tmp->suiv = som;
+    
+    return tmp;
 }
 
-void detruit_liste_somme(Cellule_som* cs){
-	Cellule_som ite = cs;
-	while(ite){
-		Cellule_som tmp=ite;
-		ite=ite->suiv;
-		free(tmp);
-	}
+
+void detruit_liste_somme(Cellule_som *som){
+
+    Cellule_Sommet *tmp = som;
+    
+    while(tmp){ //tant tmp ne vaut pas null
+        som = tmp->suiv; // on stock la suite de la liste
+        
+        free(tmp); // on désalloue l'element courant de la liste 
+        tmp = som; // on recupere dans tmp la liste stocker dans som 
+    
+    }
+
 }
 
-void ajoute_voisin(Sommet* s1, Sommet* s2){
-	s1 -> sommet_adj = ajoute_liste_sommet(s2,s1->sommet_adj);
-	s2 -> sommet_adj = ajoute_liste_sommet(s1,s2->sommet_adj);
+
+void ajoute_voisin(Sommet *s1, Sommet *s2){
+    
+    s1->sommet_adj = ajoute_liste_sommet(s2, s1->sommet_adj); // ajout le sommet s2 dans la liste des sommets adjacents de s1
+    s2->sommet_adj = ajoute_liste_sommet(s1, s2->sommet_adj);  // ajout le sommet s1 dans la liste des sommets adjacents de s2
+
 }
 
-int adjacent(Sommet* s1, Sommet* s2){
-	Cellule_som ite1 = s1->sommet_adj;
-	while(ite1){
-		if(ite1->sommet == s2){
-			return 1;
-		}
-		ite1 = ite1->suiv;
-	}
-
-	return 0;
+int adjacent(Sommet *s1, Sommet *s2){
+    int s1_s2 = 0; //boolean initialiser à 0 pour dire que s2 n'est pas adjacent à s1 
+    int s2_s1 = 0; //boolean initialiser à 0 pour dire que s1 n'est pas adjacent à s2
+    
+    Cellule_som * tmp = s1->sommet_adj; // recupere la liste adjacent de s1
+    
+   
+    while(tmp){  //parcours dans la liste adjacent de s1
+        if(tmp->sommet == s2){ // s2 est adjacent à s1
+            s1_s2 = 1; //on met le boolean a 1
+            break;
+        }
+        
+        tmp = tmp->suiv;
+    }
+    
+    tmp = s2->sommet_adj; // recupere la liste adjacent de s2
+    
+    while(tmp){ //parcours dans la liste adjacent de s2
+        if(tmp->sommet == s1){ // s1 est adjacent à s2
+            s2_s1 = 1; //on met le boolean a 1
+            break;
+        }
+        
+        tmp = tmp->suiv;
+    }
+    
+    return (s1_s2 && s2_s1) ? 1 : 0;
 }
-
 
